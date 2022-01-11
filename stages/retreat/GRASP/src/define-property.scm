59f7fc64ad644c7fb514e11f9930d1b7c9ac2094
Panicz Maciej Godek <maciej.godek@kayon.pl>
2022-01-11 01:39:31 +0100

    Avoiding double evaluation of expressions passed to properties

--- stages/retreat/GRASP/src/define-property.scm
+++ stages/retreat/GRASP/src/define-property.scm
@@ -2,16 +2,20 @@
 (import (define-syntax-rule))
 (import (hash-table))
 
+(define-syntax-rule (property (object) default)
+  (let* ((override (make-weak-key-hash-table))
+         (getter (lambda (object)
+                   (hash-ref override object
+                             (lambda () default)))))
+    (set! (setter getter) (lambda (arg value)
+                            (hash-set! override arg value)))
+    getter))
+
 (define-syntax-rule (define-property (property-name object) default)
   (define property-name
-    (let* ((override (make-weak-key-hash-table))
-	   (getter (lambda (object)
-		     (hash-ref override object
-			       (lambda () default)))))
-      (set! (setter getter) (lambda (arg value)
-			      (hash-set! override arg value)))
-      getter)))
+    (property (object) default)))
 
-(define-syntax-rule (update! (property object) value)
-  (unless (equal? (property object) value)
-    (set! (property object) value)))
+(define-syntax-rule (update! (property object) expression)
+  (let ((value expression))
+    (unless (equal? (property object) value)
+      (set! (property object) value))))
