f69abfaca80867d71636e58ba49e1f40c2cfb6e8
Panicz <godek.maciek@gmail.com>
2022-01-31 20:00:36 +0100

    Rudimentary support for mouse

--- stages/retreat/GRASP/src/editor.scm
+++ stages/retreat/GRASP/src/editor.scm
@@ -35,66 +35,104 @@
 
 (define (run-editor #!optional (io ::Terminal (make-terminal)))::void
   (io:enterPrivateMode)
-  (let continue ()
-    (io:clearScreen)
-    (io:setCursorPosition 0 0)
-    (io:putString ((current-screen):toString))
-    (io:setCursorPosition 0 (+ 2 output-extent:height))
-    (io:putString (with-output-to-string (lambda () (write cursor))))
-    (io:setCursorPosition 0 (+ 4 output-extent:height))
-    (try-catch
-     (begin
-       (io:putString (show->string (cursor-ref document cursor))))
-     (ex java.lang.Throwable
-	 (io:putString (ex:toString))))
-    (io:flush)
-    (let* ((key ::KeyStroke (io:readInput))
-	   (type ::KeyType (key:getKeyType)))
-             
-      (match type	
-	(,KeyType:ArrowLeft
-	 (try-catch
-	  (set! cursor (cursor-climb-back
-			(cursor-back cursor document)
-			document))
-	  (ex java.lang.Throwable
-	      (io:putString (ex:toString))))
-	 (continue))
-	
-	(,KeyType:ArrowRight
-	 (try-catch
-	  (set! cursor (cursor-climb-front
-			(cursor-next cursor document)
-			document))
-	  (ex java.lang.Throwable
-	      (io:putString (ex:toString))))
-	 (continue))
-	
-	(,KeyType:ArrowUp
-	 (try-catch
-	  (set! cursor (cursor-climb-front cursor document))
-	  (ex java.lang.Throwable
-	      (io:putString (ex:toString))))
-	 (continue))
-	
-	(,KeyType:ArrowDown
-	 (try-catch
-	  (set! cursor (cursor-climb-back cursor document))
-	  (ex java.lang.Throwable
-	      (io:putString (ex:toString))))
-	 (continue))
-	
-	(,KeyType:EOF
-	 (values))
+  (when (instance? io ExtendedTerminal)
+    ((as ExtendedTerminal io):setMouseCaptureMode
+     MouseCaptureMode:CLICK_RELEASE_DRAG))
+  (let ((warning ""))
+    (let continue ()
+      (io:clearScreen)
+      (io:setCursorPosition 0 0)
+      (io:putString ((current-screen):toString))
+      (io:setCursorPosition 0 (+ 2 output-extent:height))
+      (io:putString (with-output-to-string (lambda () (write cursor))))
+      (io:setCursorPosition 0 (+ 4 output-extent:height))
+      (try-catch
+       (begin
+	 (io:putString (show->string (cursor-ref document cursor))))
+       (ex java.lang.Throwable
+	   (io:putString (ex:toString))))
+      (io:putString warning)
+      (io:flush)
+      (set! warning "")
+      (let* ((key ::KeyStroke (io:readInput))
+	     (type ::KeyType (key:getKeyType)))
+        
+	(match type	
+	  (,KeyType:ArrowLeft
+	   (try-catch
+	    (set! cursor (cursor-climb-back
+			  (cursor-back cursor document)
+			  document))
+	    (ex java.lang.Throwable
+		(set! warning (ex:toString))))
+	   (continue))
+	  
+	  (,KeyType:ArrowRight
+	   (try-catch
+	    (set! cursor (cursor-climb-front
+			  (cursor-next cursor document)
+			  document))
+	    (ex java.lang.Throwable
+		(set! warning (ex:toString))))
+	   (continue))
+	  
+	  (,KeyType:ArrowUp
+	   (try-catch
+	    (set! cursor (cursor-climb-front cursor document))
+	    (ex java.lang.Throwable
+		(set! warning (ex:toString))))
+	   (continue))
+	  
+	  (,KeyType:ArrowDown
+	   (try-catch
+	    (set! cursor (cursor-climb-back cursor document))
+	    (ex java.lang.Throwable
+		(set! warning (ex:toString))))
+	   (continue))
+	  
+	  (,KeyType:EOF
+	   (values))
 
-	(,KeyType:Character
-	 (continue))
-	
-	(_
-	 (continue))
-	)
-      ))
-  (io:exitPrivateMode)
-  (io:close))
+	  (,KeyType:Character
+	   (continue))
+
+	  (,KeyType:MouseEvent
+	   (let* ((action ::MouseAction (as MouseAction key))
+		  (position ::TerminalPosition (action:getPosition)))
+	     (cond ((action:isMouseMove)
+		    (set! warning
+			  (string-append "mouse move to "
+					 (position:toString))))
+		   ((action:isMouseDown)
+		    (match (action:getButton)
+		      (,MouseButton:Left
+		       (set! warning
+			     (string-append "left mouse at "
+					    (position:toString))))
+		      (,MouseButton:Right
+		       (set! warning
+			     (string-append "right mouse at "
+					    (position:toString))))
+		      (_
+		       (set! warning
+			     (string-append (action:toString)
+					    " mouse at "
+					    (position:toString))))))
+		   ((action:isMouseDrag)
+		    (set! warning
+			  (string-append "mouse move to "
+					 (position:toString))))
+		   ((action:isMouseUp)
+		    (set! warning
+			  (string-append "mouse released at "
+					 (position:toString))))))
+	   (continue))
+	  
+	  (_
+	   (continue))
+	  )
+	))
+    (io:exitPrivateMode)
+    (io:close)))
 
 (run-editor)
--- stages/retreat/GRASP/src/term.scm
+++ stages/retreat/GRASP/src/term.scm
@@ -7,6 +7,12 @@
 (define-alias TerminalSize
   com.googlecode.lanterna.TerminalSize)
 
+(define-alias MouseCaptureMode
+  com.googlecode.lanterna.terminal.MouseCaptureMode)
+
+(define-alias ExtendedTerminal
+  com.googlecode.lanterna.terminal.ExtendedTerminal)
+
 (define-alias TextColor com.googlecode.lanterna.TextColor)
 
 (define-alias Terminal com.googlecode.lanterna.terminal.Terminal)
@@ -14,6 +20,15 @@
 (define-alias KeyStroke com.googlecode.lanterna.input.KeyStroke)
 
 (define-alias KeyType com.googlecode.lanterna.input.KeyType)
+
+(define-alias MouseAction com.googlecode.lanterna.input.MouseAction)
+
+(define MouseButton:None ::int 0)
+(define MouseButton:Left ::int 1)
+(define MouseButton:Middle ::int 2)
+(define MouseButton:Right ::int 3)
+(define MouseButton:WheelUp ::int 4)
+(define MouseButton:WheelDown ::int 5)
 
 (define defaultTerminalFactory
   ::DefaultTerminalFactory (DefaultTerminalFactory))
