915a5a9f999809c589d3afdfedebd7d3c3f2e422
Panicz <godek.maciek@gmail.com>
2022-02-22 08:08:57 +0100

    A custom current-message-handler for editor

--- stages/retreat/GRASP/src/editor.scm
+++ stages/retreat/GRASP/src/editor.scm
@@ -29,12 +29,8 @@
 
 (define document ::list (parse-string input))
 
-;; powinien byc raczej TerminalScreen
-(set! (current-screen) (TextScreen))
 
 (define input-extent ::Extent (string-extent input))
-
-(define output-extent ::Extent (draw! (head document)))
 
 (define cursor ::Cursor '())
 
@@ -58,9 +54,7 @@
       (for message in messages
 	(io:putString message))))
 
-  (Object)
   (set! history-length size))
-    
 
 (define (run-editor #!optional (io ::Terminal (make-terminal)))::void
   (io:enterPrivateMode)
@@ -68,91 +62,96 @@
     ((as ExtendedTerminal io):setMouseCaptureMode
      MouseCaptureMode:CLICK_RELEASE_DRAG))
 
-  (parameterize ((current-message-handler (editor-message-handler 10)))
+  (parameterize ((current-message-handler (editor-message-handler 22))
+		 (current-screen (TextScreen)))
   
     (let continue ()
-      (io:setCursorVisible #f)
-      (io:clearScreen)
-      (io:setCursorPosition 0 0)
-      (io:putString ((current-screen):toString))
-      (io:setCursorPosition 0 (+ 2 output-extent:height))
-      (io:putString (with-output-to-string (lambda () (write cursor))))
-      (io:setCursorPosition 0 (+ 4 output-extent:height))
-      (try-catch
-       (begin
-	 (io:putString (show->string (cursor-ref document cursor))))
-       (ex java.lang.Throwable
-	   (WARN (ex:toString))))
-      (invoke (current-message-handler) 'display-messages io)
-      (io:flush)
-      (io:setCursorVisible #t)
-      (let* ((key ::KeyStroke (io:readInput))
-	     (type ::KeyType (key:getKeyType)))
-	(match type	
-	  (,KeyType:ArrowLeft
-	   (try-catch
-	    (set! cursor (cursor-climb-back
-			  (cursor-back cursor document)
-			  document))
-	    (ex java.lang.Throwable
-		(WARN (ex:toString))))
-	   (continue))
-	  
-	  (,KeyType:ArrowRight
-	   (try-catch
-	    (begin
-	      (set! cursor (cursor-climb-front
-			    (cursor-next cursor document)
-			    document)))
-	    (ex java.lang.Throwable
-		(WARN (ex:toString))))
-	   (continue))
-	  
-	  (,KeyType:ArrowUp
-	   (try-catch
-	    (set! cursor (cursor-climb-front cursor document))
-	    (ex java.lang.Throwable
-		(WARN (ex:toString))))
-	   (continue))
-	  
-	  (,KeyType:ArrowDown
-	   (try-catch
-	    (set! cursor (cursor-climb-back cursor document))
-	    (ex java.lang.Throwable
-		(WARN (ex:toString))))
-	   (continue))
-	  
-	  (,KeyType:EOF
-	   (values))
+      (let* ((output-extent ::Extent (draw! (head document)
+					    cursor: cursor
+					    context: (recons 1 '()))))
 
-	  (,KeyType:Character
-	   (continue))
+	(io:setCursorVisible #f)
+	(io:clearScreen)
+	(io:setCursorPosition 0 0)
+	(io:putString ((current-screen):toString))
+	(io:setCursorPosition 0 (+ 2 output-extent:height))
+	(io:putString (with-output-to-string (lambda () (write cursor))))
+	(try-catch
+	 (io:putString (with-output-to-string
+			 (lambda ()
+			   (write (cursor-ref document cursor)))))
+	 (ex java.lang.Throwable
+	     (WARN (ex:toString))))
+	(invoke (current-message-handler) 'display-messages io)
+	(io:flush)
+	(io:setCursorVisible #t)
+	(let* ((key ::KeyStroke (io:readInput))
+	       (type ::KeyType (key:getKeyType)))
+	  (match type	
+	    (,KeyType:ArrowLeft
+	     (try-catch
+	      (set! cursor (cursor-climb-back
+			    (cursor-back cursor document)
+			    document))
+	      (ex java.lang.Throwable
+		  (WARN (ex:toString))))
+	     (continue))
+	    
+	    (,KeyType:ArrowRight
+	     (try-catch
+	      (begin
+		(set! cursor (cursor-climb-front
+			      (cursor-next cursor document)
+			      document)))
+	      (ex java.lang.Throwable
+		  (WARN (ex:toString))))
+	     (continue))
+	    
+	    (,KeyType:ArrowUp
+	     (try-catch
+	      (set! cursor (cursor-climb-front cursor document))
+	      (ex java.lang.Throwable
+		  (WARN (ex:toString))))
+	     (continue))
+	    
+	    (,KeyType:ArrowDown
+	     (try-catch
+	      (set! cursor (cursor-climb-back cursor document))
+	      (ex java.lang.Throwable
+		  (WARN (ex:toString))))
+	     (continue))
+	    
+	    (,KeyType:EOF
+	     (values))
 
-	  (,KeyType:MouseEvent
-	   (let* ((action ::MouseAction (as MouseAction key))
-		  (position ::TerminalPosition (action:getPosition)))
-	     (cond ((action:isMouseMove)
-		    (WARN "mouse move to " (position:toString)))
-		   ((action:isMouseDown)
-		    (match (action:getButton)
-		      (,MouseButton:Left
-		       (WARN "left mouse at " (position:toString)))
-		      (,MouseButton:Right
-		       (WARN "right mouse at " (position:toString)))
-		      (_
-		       (WARN (action:toString) " mouse at "
-			     (position:toString)))))
-		   ((action:isMouseDrag)
-		    (WARN "mouse move to " (position:toString)))
-		   ((action:isMouseUp)
-		    (WARN "mouse released at "
-			  (position:toString)))))
-	   (continue))
-	  
-	  (_
-	   (continue))
-	  )
-	))
+	    (,KeyType:Character
+	     (continue))
+
+	    (,KeyType:MouseEvent
+	     (let* ((action ::MouseAction (as MouseAction key))
+		    (position ::TerminalPosition (action:getPosition)))
+	       (cond ((action:isMouseMove)
+		      (WARN "mouse move to " (position:toString)))
+		     ((action:isMouseDown)
+		      (match (action:getButton)
+			(,MouseButton:Left
+			 (WARN "left mouse at " (position:toString)))
+			(,MouseButton:Right
+			 (WARN "right mouse at " (position:toString)))
+			(_
+			 (WARN (action:toString) " mouse at "
+			       (position:toString)))))
+		     ((action:isMouseDrag)
+		      (WARN "mouse move to " (position:toString)))
+		     ((action:isMouseUp)
+		      (WARN "mouse released at "
+			    (position:toString)))))
+	     (continue))
+	    
+	    (_
+	     (continue))
+	    )
+	  )))
     (io:exitPrivateMode)
     (io:close)))
 
