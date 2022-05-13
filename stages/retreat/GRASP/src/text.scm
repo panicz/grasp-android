1f9411319858628f1a92145a68eddb72b701d3bf
Panicz <godek.maciek@gmail.com>
2022-05-13 16:32:55 +0200

    Adding Text object to represent strings

---
+++ stages/retreat/GRASP/src/text.scm
@@ -0,0 +1,68 @@
+(import (define-syntax-rule))
+(import (assert))
+(import (define-interface))
+(import (define-type))
+(import (define-object))
+(import (define-property))
+(import (define-cache))
+(import (match))
+(import (examples))
+(import (infix))
+(import (extent))
+(import (indexable))
+(import (space))
+(import (cursor))
+(import (tile))
+(import (for))
+(import (screen))
+(import (functions))
+(import (print))
+
+
+(define-object (Text)::Tile
+  (define (draw! screen::Screen
+		 cursor::Cursor
+		 context::Cursor
+		 anchor::Cursor)
+    (screen:draw-string! (this) 0 0))
+
+  (define (extent screen::Screen)::Extent
+    (let ((inner ::Extent (string-extent (this))))
+      (Extent width: (+ inner:width 2)
+	      height: (max (screen:min-line-height)
+			   inner:height))))
+
+  (define (part-at index::Index)::Indexable*
+    (this))
+
+  (define (first-index)::Index
+    #\[)
+   
+  (define (last-index)::Index
+    #\])
+
+  (define (next-index index::Index)::Index
+    (match index
+      (,(first-index) 0)
+      (,(last-index) (last-index))
+      (,@(is _ < (string-length (this)))
+       (+ index 1))
+      (_
+       (last-index))))
+  
+  (define (previous-index index::Index)::Index
+    (match index
+      (0 (first-index))
+      (,(last-index) (string-length (this)))
+      (,(first-index) (first-index))
+      (_ (- index 1))))
+
+  (define (index< a::Index b::Index)::boolean
+    (or (and (is a eqv? (first-index))
+	     (isnt b eqv? (first-index)))
+	(and (number? a) (number? b)
+	     (is a < b))
+	(and (is b eqv? (last-index))
+	     (isnt a eqv? (last-index)))))
+
+  (gnu.lists.FString))
