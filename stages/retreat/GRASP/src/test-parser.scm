0450ae2147fa8af5d819ca8a39026c08cb35dda8
Panicz <godek.maciek@gmail.com>
2022-05-14 09:41:30 +0200

    Added some tests for line-comments

--- stages/retreat/GRASP/src/test-parser.scm
+++ stages/retreat/GRASP/src/test-parser.scm
@@ -3,7 +3,10 @@
  (primitive)
  (parse)
  (examples)
-  )
+ (conversions)
+ (srfi :11)
+ (assert)
+ )
 
 (define factorial-definition (parse-string "\
 (define (factorial n)
@@ -23,3 +26,31 @@
 
 (show (car string-append-example))
 (newline)
+
+(define something-with-line-comments (parse-string "\
+; this input contains line comments
+( ; in various places 
+ + ; like, after an operator
+ 2 ; and an operand
+ 3 ; and at the end of a list
+)
+
+( ;as well as in an empty list
+)
+
+;and at the end of input
+"))
+
+(show something-with-line-comments)
+(newline)
+
+
+;; sadly, it doesn't work for empty input
+
+(define line-comments-with-empty-input (parse-string "\
+; line comments with empty input
+"))
+
+(show line-comments-with-empty-input)
+(newline)
+
