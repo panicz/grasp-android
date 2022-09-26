4f6e0e334e01e14029af111b6abf7b1038805607
Panicz <godek.maciek@gmail.com>
2022-09-26 06:50:50 +0200

    Improved error messages for invalid examples

--- stages/retreat/GRASP/src/examples.scm
+++ stages/retreat/GRASP/src/examples.scm
@@ -1,3 +1,14 @@
+
+(define (bad-example form expected-value obtained-value)
+  (call-with-output-string
+   (lambda (port)
+     (display "while evaluating\n" port)
+     (display form port)
+     (display "\n\nExpected:\n" port)
+     (display expected-value port)
+     (display "\nGot: \n" port)
+     (display obtained-value port))))
+  
 (define-syntax e.g.
   (syntax-rules (===> $bracket-list$ $string$)
     ((_ example)
@@ -8,19 +19,20 @@
      (let ((result example))
        (if (equal? result value)
 	   result
-	   (error '(example ===> value) result))))
+	   (error (bad-example 'example 'value result))
+	   )))
 
     ((_ example ===> ($string$ . value))
      (let ((result example))
        (if (equal? result ($string$ . value))
 	   result
-	   (error `(example ===> ,($string$ . value)) result))))
+	   (error(bad-example 'example ($string$ value) result)))))
     
     ((_ example ===> value)
      (let ((result example))
        (if (equal? result 'value)
 	   result
-	   (error '(example ===> value) result))))
+	   (error (bad-example 'example 'value result)))))
 
     ((_ example ===> value ...)
      (call-with-values (lambda () example)
